a591fc12c6f92fb63188711878d0e0f6af29b3c4
ANightDazingZoroark <anightdazingzoroark22@gmail.com>
2026-06-22 18:23:08 +0800

    versioning fix

--- src/main/java/org/valkyrienskies/mod/common/block/IBlockBuoyancyProvider.java
+++ src/main/java/org/valkyrienskies/mod/common/block/IBlockBuoyancyProvider.java
@@ -5,6 +5,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.valkyrienskies.mod.common.ships.ship_world.PhysicsObject;
 
+//note: buoyancy is a force, maybe put it in ShipData.activeForcePositions?
 public interface IBlockBuoyancyProvider {
     /**
      * returns how many full water blocks this block displaces when fully submerged
